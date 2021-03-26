d4a30dc3d30e2a6589df1224de81ff6669e8e981
limiu331 <845217637@qq.com>
2021-03-26 17:39:31 +0800

    add accountSelect

--- src/components/Header/SelectAccount/index.tsx
+++ src/components/Header/SelectAccount/index.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import {SelectAccountStyle} from "./style";
-
+import buttonLogo from './icons/Account Settings.svg'
+import {Modal,Radio} from "antd";
 function SelectAccount(){
+    const [AccountListModal,SetAccountListModal] =useState(false)
+    const [value, setValue] = React.useState(1);
+    const onChange = e => {
+        console.log('radio checked', e.target.value);
+        setValue(e.target.value);
+    };
+
     return (
         <SelectAccountStyle>
-            <div>
-                <div>Sanne Viscaal</div>
-                <div>dasdsadasd</div>
-            </div>
-            <div>按钮</div>
+            <div className={"current-account"}>5RkTK…c31Cr</div>
+            <img src={buttonLogo} alt="" onClick={()=>SetAccountListModal(true)}/>
+           <Modal title={"请选择账号"} visible={AccountListModal} onCancel={()=>SetAccountListModal(false)} getContainer={false}>
+               <Radio.Group onChange={onChange} value={value}>
+                   <Radio value={1}>
+                       <div className={"account-info"}>
+                           <div className={"account"}>BBB</div>
+                           <div className={"balance"}>余额 500 PCX</div>
+                       </div>
+                   </Radio>
+               </Radio.Group>
+           </Modal>
         </SelectAccountStyle>
     )
 }
