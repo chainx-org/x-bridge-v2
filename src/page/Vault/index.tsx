97171fee1c7bee8279e226b9f15a911758c0de96
limiu <845271637@qq.com>
2021-03-31 16:24:15 +0800

    add vault status

--- src/page/Vault/index.tsx
+++ src/page/Vault/index.tsx
@@ -1,12 +1,29 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import RegisterVaultCard from "../../components/RegisterVaultCard";
 import VaultCard from "../../components/VaultCard";
-function Vault(){
+import { Option } from "@polkadot/types";
+import {Vault} from "../../interfaces"
+import useAccountModel from "../../hooks/useAccountModel"
+import {useApi} from "../../hooks/useApi"
+import { createModel } from "hox";
+const useVaultModel = createModel(() => useState<Vault | null>(null));
+function VaultPage(){
+    const {currentAccount} = useAccountModel();
+    const { api, isApiReady } = useApi();
+    const [vault, setVault] = useVaultModel();
+    // 查询 vault 状态
+    useEffect(()=> {
+        async function GetStatus() {
+            const status = await api.query.xGatewayBitcoinV2.vaults<Option<Vault>>(currentAccount?.address || "");
+            setVault(status.isNone ? null : (status.value as Vault));
+        }
+        GetStatus();
+    },[currentAccount])
     return (
         <>
-            {/*<RegisterVaultCard/>*/}
-            <VaultCard/>
+            {vault ? <VaultCard/> : <RegisterVaultCard/>}
+            {/*<VaultCard/>*/}
         </>
     )
 }
-export default Vault;
+export default VaultPage;
