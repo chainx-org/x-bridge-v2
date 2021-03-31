7977181ededecb5a1230fd3505dd5da7a2272f3d
limiu <845271637@qq.com>
2021-03-31 16:23:46 +0800

    add account select

--- src/components/Header/SelectAccount/index.tsx
+++ src/components/Header/SelectAccount/index.tsx
@@ -1,39 +1,47 @@
-import React, { useState } from "react";
-import { SelectAccountStyle } from "./style";
+import React, {useState} from "react";
+import {SelectAccountStyle} from "./style";
 import buttonLogo from "./icons/Account Settings.svg";
-import { Modal, Radio } from "antd";
+import {Modal, Radio} from "antd";
 import useAccountModel from "../../../hooks/useAccountModel";
-function SelectAccount() {
-  const [AccountListModal, SetAccountListModal] = useState(false);
-  const [value, setValue] = useState(1);
-  const { currentAccount } = useAccountModel();
 
-  return (
-    <SelectAccountStyle>
-      <div className={"current-account"}>5RkTK…c31Cr</div>
-      <img src={buttonLogo} alt="" onClick={() => SetAccountListModal(true)} />
-      <Modal
-        title={"请选择账号"}
-        visible={AccountListModal}
-        onCancel={() => SetAccountListModal(false)}
-        getContainer={false}
-      >
-        <Radio.Group
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-          value={value}
-        >
-          <Radio value={1}>
-            <div className={"account-info"}>
-              <div className={"account"}>BBB</div>
-              <div className={"balance"}>余额 500 PCX</div>
-            </div>
-          </Radio>
-        </Radio.Group>
-      </Modal>
-    </SelectAccountStyle>
-  );
+function SelectAccount() {
+    const accountModel = useAccountModel();
+    const [AccountListModal, SetAccountListModal] = useState(false);
+    const {currentAccount} = useAccountModel();
+    const {accounts} = useAccountModel();
+    const [value, setValue] = useState(currentAccount);
+    const currentAddress = currentAccount?.address
+    return (
+        <SelectAccountStyle>
+            <div className={"current-account"}>{currentAccount?.name}</div>
+            <img src={buttonLogo} alt="" onClick={() => SetAccountListModal(true)}/>
+            <Modal
+                title={"请选择账号"}
+                visible={AccountListModal}
+                onCancel={() => SetAccountListModal(false)}
+                getContainer={false}
+                footer={null}
+            >
+                <Radio.Group
+                    onChange={(e) => {
+                        setValue(e.target.value);
+                        const changeAccount = accounts.filter((item) => item.address === e.target.value?.toString())
+                        accountModel.setCurrentAccount({name:changeAccount[0].name,address:changeAccount[0].address})
+                    }}
+                    value={value}
+                    defaultValue={currentAddress}
+                >
+                    {
+                        accounts.map((account)=>
+                            <Radio value={account.address} key={account.address}>
+                                <div>{account.name}</div>
+                            </Radio>
+                        )
+                    }
+                </Radio.Group>
+            </Modal>
+        </SelectAccountStyle>
+    );
 }
 
 export default SelectAccount;
